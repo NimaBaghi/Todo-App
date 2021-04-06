543588d4d264d6f14c4d2cb30e6a65c1ef56bc6e
Nima Baghi <nimaba1999@gmail.com>
2021-04-06 19:43:07 +0430

    add static list

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,34 @@ import React from 'react';
 import './App.css';
 
 function App(): React.ReactElement {
+  const items: Array<{
+    id: number;
+    task: string;
+    status: string;
+    date: Date;
+  }> = [
+    { id: 1, task: 'Todo 1', status: 'Complete', date: new Date() },
+    {
+      id: 1,
+      task: 'Todo 2',
+      status: 'In Progress',
+      date: new Date('2020-01-16'),
+    },
+    { id: 3, task: 'Todo 3', status: 'Complete', date: new Date() },
+    { id: 4, task: 'Todo 4', status: 'Complete', date: new Date() },
+  ];
+
+  // const [items, setitems] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Test</p>
-      </header>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.task} {item.status} {item.date.getTime()}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
